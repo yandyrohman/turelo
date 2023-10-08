934d5e4a56a141f0a208d56ac970b4dcba60aa82
Yandi Rohman <yandy.rhm@gmail.com>
2023-10-08 21:22:30 +0700

    employee

---
+++ src/components/employee/person.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="w-full bg-white flex px-5 select-none items-center py-4 border-b hover:bg-zinc-50">
+    <div class="shrink-0 w-[25%] flex gap-4 pr-3 items-center">
+      <img
+        class="shrink-0 w-[40px] h-[40px] rounded-full"
+        draggable="false"
+        src="https://www.jamsadr.com/images/neutrals/person-donald-900x1080.jpg"
+      >
+      <div class="w-full">
+        <div class="w-full font-semibold line-clamp-1 break-all">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil quibusdam delectus ea asperiores incidunt quis possimus temporibus vero, amet natus aspernatur reiciendis, maxime dignissimos dolor quia, illo laboriosam architecto dolorem?
+          <!-- Yandi Rohman -->
+        </div>
+        <div class="text-sm text-zinc-500 line-clamp-1 break-all">
+          yandy.rhm@gmail.com
+          <!-- Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque consectetur labore quia recusandae eaque. Sequi corrupti vitae, perspiciatis laudantium, natus ut rerum optio nobis dolorem necessitatibus dolore incidunt eligendi sed. -->
+        </div>
+      </div>
+    </div>
+    <div class="shrink-0 w-[16.25%]">
+      Frontend Dev
+    </div>
+    <div class="shrink-0 w-[16.25%]">
+      Rp 100.000.000
+    </div>
+    <div class="shrink-0 w-[16.25%]">
+      Karyawan Tetap
+    </div>
+    <div class="shrink-0 w-[16.25%]">
+      Admin
+    </div>
+    <div class="shrink-0 w-[10%] flex gap-4">
+      <div class="w-[40px] h-[30px] flex justify-center items-center hover:bg-zinc-200 rounded-md cursor-pointer">
+        <icon icon="tabler:edit" class="text-xl text-zinc-500" />
+      </div>
+      <div class="w-[40px] h-[30px] flex justify-center items-center hover:bg-zinc-200 rounded-md cursor-pointer">
+        <icon icon="tabler:trash" class="text-xl text-zinc-500" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import { Icon } from '@iconify/vue'
+</script>
--- src/pages/Employee.vue
+++ src/pages/Employee.vue
@@ -1,5 +1,23 @@
 <template>
-  ini list karwaan
+  <div class="w-full h-max pb-[50px]">
+    <div class="w-full h-[100px] flex justify-center items-center font-semibold select-none text-xl">List Karyawan</div>
+    <div class="w-full flex justify-center">
+      <div class="w-[1100px] h-max">
+        <div class="w-full h-max rounded-md overflow-hidden shadow-md">
+          <div class="w-full flex py-3 bg-zinc-200 select-none px-5">
+            <div class="w-[25%] text-zinc-500">Nama Karyawan</div>
+            <div class="w-[16.25%] text-zinc-500">Jabatan</div>
+            <div class="w-[16.25%] text-zinc-500">Gaji</div>
+            <div class="w-[16.25%] text-zinc-500">Status</div>
+            <div class="w-[26.25%] text-zinc-500">Role System</div>
+          </div>
+          <person-employee v-for="i in 5" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+  import personEmployee from '../components/employee/person.vue';
+</script>
