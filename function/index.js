cddf3617386e9e8453ce7a23842923f33c5be98b
Yandi Rohman <yandy.rhm@gmail.com>
2023-10-10 22:18:10 +0700

    backend

---
+++ function/index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => res.send('turelo backend'))
+
+app.listen(8080, () => console.log('App running on port 8080'))
