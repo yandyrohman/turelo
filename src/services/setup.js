17da21a96cfe3b66796fac277b1e8ff9e32ec50f
Yandi Rohman <yandy.rhm@gmail.com>
2024-01-26 07:57:04 +0700

    fix for firebase

--- src/services/setup.js
+++ src/services/setup.js
@@ -1,2 +1,2 @@
-export const baseUrl = 'http://localhost:8080'
-// export const baseUrl = 'https://us-central1-work-management-yandi.cloudfunctions.net/app'
+// export const baseUrl = 'http://localhost:8080'
+export const baseUrl = 'https://us-central1-work-management-yandi.cloudfunctions.net/app'
